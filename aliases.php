017a65b2a78699f677dab985d8851ea392766a56
dbojdo <daniel@bojdo.eu>
2025-11-10 18:07:31 +0000

    [FIX] ReDeprecations

--- aliases.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// Backward compatibility with DBAL 2.x
-if(!class_exists(\Doctrine\DBAL\Exception::class)) {
-    class_alias(\Doctrine\DBAL\DBALException::class, \Doctrine\DBAL\Exception::class);
-}
